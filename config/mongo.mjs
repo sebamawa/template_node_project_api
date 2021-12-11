80840f5352c57330c228eade6bda1deab52604db
sebamawa <sebamawa@hotmail.com>
2021-12-11 16:59:24 -0300

    add atlas mongoDB credentials

--- config/mongo.mjs
+++ config/mongo.mjs
@@ -1,21 +1,43 @@
 // conexion a base de datos
 import mongoose from 'mongoose';
 
-const dbConnect = () => {
-    console.log('Conectando a la base de datos...');
+// Conexion remota a MongoDB usando Atlas
+const dbConnect = () => { 
     const DB_URI = process.env.DB_URI;
-
-    mongoose.connect(DB_URI,//'mongodb://127.0.0.1:27017/sessions',
-         { useNewUrlParser: true, 
-           useUnifiedTopology: true 
-    }, (err, res) => {
-        if (!err) {
-            console.log('DB Connected');
-        } else {
-            console.log(`DB Connection Error; ${err}`);
-            process.exit(1); 
-        }
-    });
+    console.log(DB_URI);
+    const connectionParams = {
+        useNewUrlParser: true,
+        //useCreateIndex: true,
+        useUnifiedTopology: true 
+    }
+    mongoose.connect(DB_URI, connectionParams)
+        .then( () => {
+            console.log('Connected to database ')
+        })
+        .catch( (err) => {
+            console.error(`Error connecting to the database. \n${err}`);
+            process.exit(1);
+        })
 }
+
+    //=================================================================================
+// Conexion local a MongoDB
+// const dbConnect = () => {
+//     console.log('Conectando a la base de datos...');
+//     const DB_URI = process.env.DB_URI;
+
+//     mongoose.connect(DB_URI,//'mongodb://127.0.0.1:27017/sessions',
+//          { useNewUrlParser: true, 
+//            useUnifiedTopology: true 
+//     }, (err, res) => {
+//         if (!err) {
+//             console.log('DB Connected');
+//         } else {
+//             console.log(`DB Connection Error; ${err}`);
+//             process.exit(1); 
+//         }
+//     });
+// }
+//=================================================================================
 
 export default dbConnect;
